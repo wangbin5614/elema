6091a4a83057a8fc2459a79aba678fde28b3e4a5
wangbin5614 <962748987@qq.com>
2017-04-07 20:14:50 +0800

    2

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,29 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+    <div class="app">
+        <v-header></v-header>
+        <div class="tab">
+            <div class="table-item">商品</div>
+            <div class="table-item">评论</div>
+            <div class="table-item">商家</div>
+        </div>
+        <div class="content">
+            I am content
+        </div>
+    </div>
 </template>
 
-<script>
-  export default {
-    name: 'app'
-  }
+<script type="text/ecmascript-6">
+    import header from './components/header/header.vue';
+    export default{
+        components: {
+            'v-header': header
+        }
+    };
 </script>
 
-<style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
+<style lang="stylus" rel="stylesheet/stylus">
+  .app
+    .tab
+      display:flex
+      width:100%
 </style>
