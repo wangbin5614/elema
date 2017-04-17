b7d559ed1084bf60c5c7907142532023c0308eb1
wangbin5614 <962748987@qq.com>
2017-04-17 17:05:38 +0800

    Default

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -33,6 +33,20 @@
             <div class="detail-wrapper clearfix">
                 <div class="detail-main">
                     <h1 class="name">{{seller.name}}</h1>
+                    <div class="star-wrapper">
+                        <star :size="48" :score="seller.score"></star>
+                    </div>
+                    <titleBar :titleBar="'优惠信息'"></titleBar>
+                    <ul v-if="seller.supports" class="supports">
+                        <li v-for="item in seller.supports" class="support-item">
+                            <span class="icon" :class="classMap[item.type]"></span>
+                            <span class="text">{{item.description}}</span>
+                        </li>
+                    </ul>
+                    <titleBar :titleBar="'商家公告'"></titleBar>
+                    <div class="bulletin">
+                        <p>{{seller.bulletin}}</p>
+                    </div>
                 </div>
             </div>
             <div class="detail-close">
@@ -43,6 +57,8 @@
 </template>
 
 <script type="text/ecmascript-6">
+    import star from 'components/star/star';
+    import title from 'components/title/title';
     export default {
         props: {
             seller: {
@@ -56,6 +72,10 @@
         },
         created() {
             this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+        },
+        components: {
+            'star': star,
+            'titleBar': title
         }
     };
 </script>
@@ -103,7 +123,7 @@
         background-size: 30px 18px;
     }
 
-    .name {
+    .title .name {
         margin-left: 6px;
         font-size: 16px;
         line-height: 18px;
@@ -146,7 +166,7 @@
         background-image: url("special_1@2x.png");
     }
 
-    .text {
+    .support .text {
         font-size: 10px;
     }
 
@@ -227,28 +247,120 @@
         z-index: 10;
     }
 
-    .detail-wrapper{
-        width:100%;
+    .detail-wrapper {
+        width: 100%;
         min-height: 100%;
     }
 
-    .detail-main{
+    .detail-main {
         margin-top: 64px;
         padding-bottom: 64px;
     }
 
-    .detail-close{
+    .detail-close {
         position: relative;
-        width:32px;
-        height:32px;
-        margin:-64px auto 0 auto;
+        width: 32px;
+        height: 32px;
+        margin: -64px auto 0 auto;
         clear: both;
         font-size: 32px;
     }
-    .name{
+
+    .detail-main .name {
         line-height: 16px;
         text-align: center;
         font-size: 16px;
         font-weight: 700;
+    }
+
+    .star-wrapper {
+        margin-top: 18px;
+        padding: 2px 0;
+        text-align: center;
+    }
+
+    .detail .title {
+        display: flex;
+        width: 80%;
+        margin: 28px auto 24px auto;
+    }
+
+    .detail .title .line {
+        flex: 1;
+        position: relative;
+        top: -6px;
+        border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+    }
+
+    .detail .title .text {
+        padding: 0 12px;
+        font-size: 14px;
+        line-height: 14px;
+        font-weight: 700;
+    }
+
+    .supports {
+        width: 80%;
+        margin: 0 auto;
+    }
+
+    .support-item {
+        padding: 0 12px;
+        margin-bottom: 12px;
+        font-size: 0;
+    }
+
+    .support-item:last-child {
+        margin-bottom: 0;
+    }
+
+    .support-item .text {
+        font-size: 12px;
+        font-weight: 200;
+        color: rgb(255, 255, 255);
+        line-height: 16px;
+    }
+
+    .supports .icon {
+        display: inline-block;
+        width: 16px;
+        height: 16px;
+        margin-right: 6px;
+        background-size: 16px 16px;
+        background-repeat: no-repeat;
+        vertical-align: top;
+    }
+
+    .supports .decrease {
+        background-image: url("decrease_2@2x.png");
+    }
+
+    .supports .discount {
+        background-image: url("discount_2@2x.png");
+    }
+
+    .supports .guarantee {
+        background-image: url("guarantee_2@2x.png");
+    }
+
+    .supports .invoice {
+        background-image: url("invoice_2@2x.png");
+    }
+
+    .supports .special {
+        background-image: url("special_2@2x.png");
+    }
+
+    .bulletin {
+        width: 80%;
+        margin: 0 auto;
+    }
+
+    .bulletin p {
+        padding: 0 12px;
+        font-size: 12px;
+        font-weight: 200;
+        color: rgb(255, 255, 255);
+        line-height: 24px;
     }
 </style>
---
+++ src/components/title/title.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="title-wrapper">
+        <div class="line"></div>
+        <div class="text">{{titleBar}}</div>
+        <div class="line"></div>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    export default {
+        props: {
+            titleBar: {
+                type: String
+            }
+        }
+    };
+</script>
+
+<style>
+    .title-wrapper {
+        display: flex;
+        width: 80%;
+        margin: 28px auto 24px auto;
+    }
+
+    .title-wrapper .line {
+        flex: 1;
+        position: relative;
+        top: -6px;
+        border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+    }
+
+    .title-wrapper .text {
+        padding: 0 12px;
+        font-size: 14px;
+        line-height: 14px;
+        font-weight: 700;
+    }
+</style>
